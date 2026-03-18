45abbbd946209364eb0a25ee5feada8e33a57eb9
JohnKing376 <johnolufemikingiyanu@gmail.com>
2026-03-18 10:48:53 -0700

    feat(docker): use env variables for docker-compose.yml

--- src/infrastructure/config/env/env.validate.ts
+++ src/infrastructure/config/env/env.validate.ts
@@ -5,7 +5,11 @@ const EnvSchema = z.object({
     .enum(['development', 'test', 'production'])
     .default('development'),
   PORT: z.coerce.number().int().min(1).max(65535).default(9999),
-  DATABASE_URL: z.string().min(1, 'DATABASE_URL is required'),
+  DB_NAME: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_USER: z.string(),
+  DB_PORT: z.coerce.number().default(5432),
+  REDIS_PORT: z.coerce.number().default(6379),
   APP_NAME: z.string(),
 });
 

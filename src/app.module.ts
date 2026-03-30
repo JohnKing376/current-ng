a56c5b819a004e87a6f59d1a57d6e82b33b8eae4
JohnKing376 <johnolufemikingiyanu@gmail.com>
2026-03-30 23:23:27 +0100

    feat(app-module): remove unwanted import

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { AppConfigModule } from '@infrastructure/config/config.module';
 import { PrismaModule } from '@infrastructure/database/prisma/prisma.module';
 import { LocationModule } from './modules/location/location.module';
 import { ReportModule } from './modules/report/report.module';
-import { ReportService } from './src/modules/report/report.service';
 
 @Module({
   imports: [
@@ -31,7 +30,6 @@ import { ReportService } from './src/modules/report/report.service';
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
     },
-    ReportService,
   ],
 })
 export class AppModule {}
